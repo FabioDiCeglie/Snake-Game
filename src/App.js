2111aea1cb236cf6b5f03f8bacb6336eb4021483
FabioDiCeglie <dcfabio96@gmail.com>
2022-05-06 10:54:35 +0200

    creating snake component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import Snake from "./Snake";
+
 function App() {
-  return <div className="game-area"></div>;
+  const [state] = useState({
+    snakeDots: [
+      [0, 0],
+      [2, 0],
+    ],
+  });
+  return (
+    <div className="game-area">
+      <Snake snakeDots={state.snakeDots} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Food.js
@@ -0,0 +1,3 @@
+export default (props) => {
+  return <div className="snake-food"></div>;
+};
